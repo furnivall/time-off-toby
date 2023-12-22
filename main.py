a44fed96d7104dc971af939452d64da99699ca03
Daniel Furnivall <danielfurnivall@gmail.com>
2023-12-22 14:47:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+bot_token = 'xoxb-4231523563443-6386108488947-SZ6oM5btBAAnxGxKFafluPO0'
+signing_secret = 'c63fae3a7f322b4fa92e921c678afdbf'
+import os
+# Use the package we installed
+from slack_bolt import App
+
+# Initialize your app with your bot token and signing secret
+app = App(
+    token= bot_token,
+    signing_secret= signing_secret
+)
+
+# New functionality
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+  try:
+    # views.publish is the method that your app uses to push a view to the Home tab
+    client.views_publish(
+      # the user that opened your app's app home
+      user_id=event["user"],
+      # the view object that appears in the app home
+      view={
+        "type": "home",
+        "callback_id": "home_view",
+
+        # body of the view
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome to your _App's Home tab_* :tada:"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "Danny is here. Angela is an old person."
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    )
+
+  except Exception as e:
+    logger.error(f"Error publishing home tab: {e}")
+# Add functionality here later
+# @app.event("app_home_opened") etc.
+
+# Ready? Start your app!
+if __name__ == "__main__":
+    app.start(port=int(os.environ.get("PORT", 3000)))
